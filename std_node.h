16a3351634adc44e671e5f8028aca2fe82b412eb
aoeiauuarar <tawangood46@gmail.com>
2026-02-02 10:41:03 +0000

    finallll

---
+++ std_node.h
@@ -0,0 +1,19 @@
+//
+//  std_node.h
+//  simple linkedlist
+//
+//  Created by Mingmanas Sivaraksa on 4/2/2566 BE.
+//
+
+#ifndef stdnode_h
+#define stdnode_h
+
+struct std_node
+{
+    int id;
+    char name[30];
+    struct std_node *next;
+};
+
+
+#endif /* std_node_h */
